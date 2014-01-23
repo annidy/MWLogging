311ed6e2787df9c5b8ba4875ea70b9172fe6056e
youxin <annidy@gmail.com>
2014-01-23 11:45:40 +0800

    Add Log File

--- MWLogging.m
+++ MWLogging.m
@@ -1,12 +1,16 @@
 // We need all the log functions visible so we set this to DEBUG
  
 #import "MWLogging.h"
- 
+#define MAX_LOG_SIZE 1024*1024
+static void AddLogFile(NSString *fname);
 static void AddStderrOnce()
 {
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		asl_add_log_file(NULL, STDERR_FILENO);
+	#if SUPPORT_LOG_FILE
+		AddLogFile([NSTemporaryDirectory() stringByAppendingPathComponent:@"MyLogFile.log"]);
+	#endif
 	});
 }
  
@@ -21,4 +25,20 @@ void _MWLog(int LEVEL, NSString *format, ...)
 	[message release];
 #endif
 	va_end(args);
+}
+
+static void AddLogFile(NSString *fname)
+{
+	int result = 1;
+	int fd = -1;
+	NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfItemAtPath:fname error:NULL];
+	unsigned long long fsize = [attrs fileSize];
+	if (fsize > MAX_LOG_SIZE) {
+		fd = open([fname fileSystemRepresentation], (O_RDWR|O_CREAT|O_TRUNC), (S_IRWXU|S_IRWXG|S_IRWXO));
+	} else {
+		fd = open([fname fileSystemRepresentation], (O_RDWR|O_CREAT|O_APPEND), (S_IRWXU|S_IRWXG|S_IRWXO));		
+	}
+	if (fd != -1) {
+		result = asl_add_log_file(NULL, fd);
+	}
 }
